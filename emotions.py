6a34ad1a1d7bbf28475a18211636802b0314c80b
Tea2Jay <itrq222@gmail.com>
2021-11-06 01:07:47 +0300

    calculating emotion each 5 frames

--- emotions.py
+++ emotions.py
@@ -1,45 +1,51 @@
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt
+import argparse
+import numpy as np
+from time import sleep
 from realTimeLatentWalk import LatentWalkerController
 
-controller = LatentWalkerController() 
+controller = LatentWalkerController()
 
-
-from time import sleep
-import numpy as np
-import argparse
-import matplotlib.pyplot as plt
-import cv2
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
 
 # Hide GPU from visible devices
 tf.config.set_visible_devices([], 'GPU')
+
 
 def plot_model_history(model_history):
     """
     Plot Accuracy and Loss curves given the model_history
     """
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     # summarize history for accuracy
-    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
-    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
+    axs[0].plot(range(1, len(model_history.history['accuracy'])+1),
+                model_history.history['accuracy'])
+    axs[0].plot(range(1, len(model_history.history['val_accuracy'])+1),
+                model_history.history['val_accuracy'])
     axs[0].set_title('Model Accuracy')
     axs[0].set_ylabel('Accuracy')
     axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+    axs[0].set_xticks(np.arange(1, len(model_history.history['accuracy'])+1),
+                      len(model_history.history['accuracy'])/10)
     axs[0].legend(['train', 'val'], loc='best')
     # summarize history for loss
-    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].plot(range(1, len(model_history.history['loss'])+1),
+                model_history.history['loss'])
+    axs[1].plot(range(1, len(model_history.history['val_loss'])+1),
+                model_history.history['val_loss'])
     axs[1].set_title('Model Loss')
     axs[1].set_ylabel('Loss')
     axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].set_xticks(np.arange(
+        1, len(model_history.history['loss'])+1), len(model_history.history['loss'])/10)
     axs[1].legend(['train', 'val'], loc='best')
     fig.savefig('plot.png')
     plt.show()
@@ -70,10 +76,12 @@ def plot_model_history(model_history):
 #         color_mode="grayscale",
 #         class_mode='categorical')
 
+
 # Create the model
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(32, kernel_size=(3, 3),
+          activation='relu', input_shape=(48, 48, 1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -98,33 +106,57 @@ cv2.ocl.setUseOpenCL(False)
 emotion_dict = {0: "Angry", 1: "Happy", 2: "Sad"}
 
 # start the webcam feed
-cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 # cap = cv2.VideoCapture("C:/Users/3izzo/Desktop/Projects/Talathum/WIN_20211021_23_20_21_Pro.mp4")
-sleep(10)
 facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+sleep(20)
 # sleep(1)
 
+frame_i = 0
+emotions = [0, 0, 0]
 while True:
     # Find haar cascade to draw bounding box around face
     ret, frame = cap.read()
     if not ret:
         break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        prediction = model.predict(cropped_img)
-        print(prediction)
-        prediction = prediction[0]
-        prediction = [prediction[0],prediction[3],prediction[5]]
-        maxindex = int(np.argmax(prediction))
-        controller.target_emotion = list(emotion_dict[maxindex].lower())[0]
-        # remove cv2 to meet our need.
-        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    faces = facecasc.detectMultiScale(
+        gray, scaleFactor=1.3, minNeighbors=5)
 
-    cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+    for (x, y, w, h) in faces:
+
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+
+        roi_gray = gray[y:y + h, x:x + w]
+
+        cropped_img = np.expand_dims(np.expand_dims(
+            cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        # print(prediction)
+
+        prediction = prediction[0]
+        prediction = [prediction[0], prediction[3], prediction[5]]
+        print(f'{prediction=}')
+
+        emotions = [emotions[0]+prediction[0], emotions[1] +
+                    prediction[1], emotions[2]+prediction[2]]
+        print(f'{emotions=}')
+            
+        if frame_i == 4:
+            emotions = [emotion/5 for emotion in emotions]
+            print(f'{emotions=} /5')
+            maxindex = int(np.argmax(emotions))
+            controller.target_emotion = list(emotion_dict[maxindex].lower())[0]
+            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            frame_i = 0
+            emotions = [0, 0, 0]
+
+        else:
+            frame_i += 1
+
+    cv2.imshow('Video', cv2.resize(
+        frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
